562362c9ca381c296ddf0eceae51bacb1cfbd622
zaynstructure <zayn.wiwchar@gmail.com>
2025-12-09 01:18:37 -0800

    Update sim_drift.glsl

--- emotional-geometry/Evf-2.3-R1/shaders/sim_drift.glsl
+++ emotional-geometry/Evf-2.3-R1/shaders/sim_drift.glsl
@@ -11,26 +11,26 @@ uniform float uBeta;
 uniform float uEta;
 uniform float uChi;
 uniform float uDelta;
-uniform vec2  uBehavior;
-uniform vec2  uContext;
-uniform vec2  uTexel;
+
+uniform vec2 uBehavior;
+uniform vec2 uContext;
+uniform vec2 uTexel;
 
 float fetchU(vec2 uv){
-  vec2 w = fract(uv);
-  float v = texture2D(uField, w).r;
-  return (v - 0.5) * 2.0;   // decode to [-1,1]
+  uv = clamp(uv, 0.001, 0.999);
+  float v = texture2D(uField, uv).r;
+  return (v - 0.5) * 2.0;
 }
 
 void main(){
   // decode drift from [0,1] -> [-1,1]
-  vec2 G = texture2D(uDrift, vUV).rg;
-  G = (G - 0.5) * 2.0;
+  vec2 G = (texture2D(uDrift, vUV).rg - 0.5) * 2.0;
 
   float uC = fetchU(vUV);
-  float uL = fetchU(vUV + vec2(-uTexel.x,0.0));
-  float uR = fetchU(vUV + vec2( uTexel.x,0.0));
-  float uD = fetchU(vUV + vec2(0.0,-uTexel.y));
-  float uU = fetchU(vUV + vec2(0.0, uTexel.y));
+  float uL = fetchU(vUV + vec2(-uTexel.x, 0.0));
+  float uR = fetchU(vUV + vec2( uTexel.x, 0.0));
+  float uD = fetchU(vUV + vec2(0.0, -uTexel.y));
+  float uU = fetchU(vUV + vec2(0.0,  uTexel.y));
 
   vec2 gradU = 0.5 * vec2(uR - uL, uU - uD);
   vec2 fieldBias = uC * gradU;
@@ -43,11 +43,11 @@ void main(){
   vec2 Gnext = G + uDT * dG;
 
   float L = length(Gnext);
-  if(L > 5.0) {
-    Gnext *= 5.0 / L;
+  if (L > 1.0) {
+    Gnext = normalize(Gnext);
   }
 
-  // re-encode [-1,1] -> [0,1] in RG
-  vec2 enc = 0.5 + 0.5 * Gnext;
-  gl_FragColor = vec4(enc, 0.0, 1.0);
+  // re-encode to [0,1]
+  vec2 outG = 0.5 + 0.5 * Gnext;
+  gl_FragColor = vec4(outG, 0.0, 1.0);
 }
